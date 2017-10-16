94e142a70086396332f927a4b73f026cfa998f9b
Vincent Bruijn <vebruijn@gmail.com>
2017-10-16 11:03:02 +0200

    authenitcation!

--- tweeter.js
+++ tweeter.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const Twitter = require('twitter');
+const config = require('./config');
 
 const tweets = './tweets.json';
 const pidFile = './pid';
@@ -7,11 +8,12 @@ const pidFile = './pid';
 let pid = 1 * fs.readFileSync(pidFile, { encoding: 'utf8' }).trim();
 let new_pid = pid + 1;
 
+
 let client = new Twitter({
-  consumer_key: 'x',
-  consumer_secret: 'y',
-  access_token_key: 'z',
-  access_token_secret: 'a'
+  consumer_key: config.consumer_key,
+  consumer_secret: config.consumer_secret,
+  access_token_key: config.access_token_key,
+  access_token_secret: config.access_token_secret
 });
 
 fs.readFile(tweets, { encoding:'utf8' }, (error, json) => {
