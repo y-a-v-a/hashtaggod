113ac515635872ad8654a5e638a26b63fa956bfd
Vincent Bruijn <vebruijn@gmail.com>
2017-10-16 11:25:00 +0200

    adds automation

--- tweeter.js
+++ tweeter.js
@@ -5,9 +5,17 @@ const config = require('./config');
 const tweets = './tweets.json';
 const pidFile = './pid';
 
-let pid = 1 * fs.readFileSync(pidFile, { encoding: 'utf8' }).trim();
-let new_pid = pid + 1;
+const tweetListAsJSON = fs.readFileSync(tweets, { encoding:'utf8' });
+const tweetList = JSON.parse(tweetListAsJSON);
 
+let pid = 1 * fs.readFileSync(pidFile, { encoding: 'utf8' }).trim();
+let new_pid;
+
+function createIntervalValue() {
+  return 2 * (40 + Math.floor(40 * Math.random())) * 60 * 1000; // tweet about every 2 hours.
+}
+
+let interval = createIntervalValue();
 
 let client = new Twitter({
   consumer_key: config.consumer_key,
@@ -16,23 +24,30 @@ let client = new Twitter({
   access_token_secret: config.access_token_secret
 });
 
-fs.readFile(tweets, { encoding:'utf8' }, (error, json) => {
-  if (error) {
-    throw error;
-  }
-
-  let tweetList = JSON.parse(json);
+function tweetFromList() {
   let tweet = tweetList[pid];
 
-  // do tweet
   client.post('statuses/update', { status: tweet },  (error, tweet, response) => {
     if (error) {
-      throw error;
+      console.log('Tweet error for ' + pid);
+      interval = createIntervalValue();
+      setTimeout(tweetFromList, interval);
+      return;
     }
+    new_pid = pid + 1;
+    console.log('Tweeted tweet ' + new_pid);
+
     fs.writeFile(pidFile, new_pid, (error) => {
       if (error) {
+        console.log('Could not write pid?');
         throw error;
+      } else {
+        pid = new_pid;
+        interval = createIntervalValue();
+        setTimeout(tweetFromList, interval);
       }
     });
   });
-});
+}
+
+setTimeout(tweetFromList, interval);
