ab20fb82fd979e1d95c6fa4aff2593aaa9499e7c
Vincent Bruijn <vebruijn@gmail.com>
2021-08-31 22:13:30 +0200

    update code

--- tweeter.js
+++ tweeter.js
@@ -1,54 +1,63 @@
-'use strict';
-const fs = require('fs');
+require('dotenv').config();
+
+const fs = require('fs/promises');
 const Twitter = require('twitter');
-const config = require('./config');
 
 const tweets = './tweets.json';
 const pidFile = './pid';
 
-const tweetListAsJSON = fs.readFileSync(tweets, { encoding:'utf8' });
-const tweetList = JSON.parse(tweetListAsJSON);
+async function main() {
+  const tweetListAsJSON = await fs.readFile(tweets, { encoding: 'utf8' });
+  const tweetList = JSON.parse(tweetListAsJSON);
 
-let pid = 1 * fs.readFileSync(pidFile, { encoding: 'utf8' }).trim();
-let new_pid;
+  let pid = 1 * (await fs.readFile(pidFile, { encoding: 'utf8' })).trim();
+  let new_pid;
 
-function createIntervalValue() {
-  return 4 * (40 + Math.floor(60 * Math.random())) * 60 * 1000; // tweet about every 5 hours.
-}
+  function createIntervalValue() {
+    return 4 * (40 + Math.floor(60 * Math.random())) * 60 * 1000; // tweet about every 5 hours.
+  }
 
-let interval = createIntervalValue();
+  let interval = createIntervalValue();
 
-let client = new Twitter({
-  consumer_key: config.consumer_key,
-  consumer_secret: config.consumer_secret,
-  access_token_key: config.access_token_key,
-  access_token_secret: config.access_token_secret
-});
-
-function tweetFromList() {
-  let tweet = tweetList[pid];
-
-  client.post('statuses/update', { status: tweet },  (error, tweet, response) => {
-    if (error) {
-      console.log('Tweet error for ' + pid);
-      interval = createIntervalValue();
-      setTimeout(tweetFromList, interval);
-      return;
-    }
-    new_pid = pid + 1;
-    console.log('Tweeted tweet ' + new_pid);
-
-    fs.writeFile(pidFile, new_pid, (error) => {
-      if (error) {
-        console.log('Could not write pid?');
-        throw error;
-      } else {
-        pid = new_pid;
-        interval = createIntervalValue();
-        setTimeout(tweetFromList, interval);
-      }
-    });
+  let client = new Twitter({
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token_key: process.env.ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
   });
+
+  function tweetFromList() {
+    let tweet = tweetList[pid];
+
+    client.post(
+      'statuses/update',
+      { status: tweet },
+      async (error, tweet, response) => {
+        if (error) {
+          console.log('Tweet error for ' + pid);
+          console.log(error);
+          interval = createIntervalValue();
+          setTimeout(tweetFromList, interval);
+          return;
+        }
+        new_pid = pid + 1;
+        console.log('Tweeted tweet ' + new_pid);
+
+        try {
+          await fs.writeFile(pidFile, `${new_pid}`);
+          pid = new_pid;
+          interval = createIntervalValue();
+          setTimeout(tweetFromList, interval);
+        } catch (e) {
+          console.log('Could not write pid?');
+          console.log(e.message);
+        }
+      }
+    );
+  }
+
+  setTimeout(tweetFromList, interval);
+  tweetFromList();
 }
 
-setTimeout(tweetFromList, interval);
+main();
